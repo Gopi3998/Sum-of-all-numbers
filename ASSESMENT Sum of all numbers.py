c87f365f164db327997eef13786034846408486d
Gopi3998 <91726200+Gopi3998@users.noreply.github.com>
2021-10-18 07:35:12 -0700

    Add files via upload

---
+++ ASSESMENT Sum of all numbers.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+print('Starting of the program for sum of all numbers'.center(80,'*'))
+def sum(*args):
+    sum1 = 0
+    for i in args:
+        sum1 = i + sum1
+    return sum1
+print(sum(8,2,3,0,7))
+print('THE END'.center(80,'*'))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
